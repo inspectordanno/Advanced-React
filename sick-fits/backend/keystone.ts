c5a3ceef4d1fe00d307fbd774349d2a1da85dc3f
Dan Spector <danno2423@gmail.com>
2021-04-04 21:49:28 -0400

    inserting seed data

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -7,6 +7,8 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -37,12 +39,18 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // show the ui only for people who pass this test
